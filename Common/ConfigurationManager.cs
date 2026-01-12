8ded97c69553473506fd4f8f080dc3c7875698aa
markld-ui <thankr3@gmail.com>
2026-01-13 00:54:03 +0700

    work on the redirect prj from winforms ro asp has been started

---
+++ Common/ConfigurationManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+
+namespace Common;
+
+public static class ConfigurationManager
+{
+    public static string GetConnectionString(string name)
+    {
+        var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings[name]?.ConnectionString;
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new ConfigurationErrorsException($"Connection string '{name}' not found in App.config.");
+        }
+
+        return connectionString;
+    }
+}
